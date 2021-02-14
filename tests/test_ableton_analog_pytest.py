43f96d5984148b59443501a5be621d99fc2339e6
Jake Spracher <jakespracher@gmail.com>
2021-02-13 19:09:16 -0500

    Extract pytest fixture to parameterize examples

--- tests/test_ableton_analog_pytest.py
+++ tests/test_ableton_analog_pytest.py
@@ -1,9 +1,16 @@
+import pytest
 from presetml.parsing import ableton_analog
 from presetml import constants
 
 
-def test_read_rewrite_should_generate_identical_presets():
-    for preset, vector, _ in ableton_analog.read_presets_from_dir(
-            constants.TEST_PRESETS_PATH)[:10]:
-        xml = ableton_analog.generate_xml_from_vector(vector)
-        assert xml.encode() == preset
+@pytest.fixture(params=ableton_analog.read_presets_from_dir(
+            constants.TEST_PRESETS_PATH)[:10])
+def test_preset_data(request):
+    return request.param
+
+
+def test_read_rewrite_should_generate_identical_presets(test_preset_data):
+    preset, vector, _ = test_preset_data
+    xml = ableton_analog.generate_xml_from_vector(vector)
+    import pdb; pdb.set_trace()
+    assert xml.encode() == preset
